aa2e4f3f4a59a69cbe7e38e29032bd319b283237
bycdiaz <cdiaz86@gmail.com>
2019-08-29 20:48:24 -0400

    Adds check_guess method.

--- ruby_on_rails/sinatra/jumpstart_labs_tutorial/web_guesser/web_guesser.rb
+++ ruby_on_rails/sinatra/jumpstart_labs_tutorial/web_guesser/web_guesser.rb
@@ -4,5 +4,13 @@ require 'sinatra/reloader'
 number = rand(100)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  erb :index, :locals => {:number => number,:message => message}
+
+  def check_guess(guess)
+    if guess.to_i > number
+      return "Too high!"
+    end
+  end
+  message = check_guess(guess)
+  
 end

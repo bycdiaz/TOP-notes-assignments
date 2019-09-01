967440d9d3f5d9a198cadb7fdeb5ab6d675cb5d3
bycdiaz <cdiaz86@gmail.com>
2019-09-01 13:18:39 -0400

    Adds ability to read names from .txt to display on page.

--- ruby_on_rails/sinatra/ruby_monstas_sinatra/monstas.rb
+++ ruby_on_rails/sinatra/ruby_monstas_sinatra/monstas.rb
@@ -5,7 +5,21 @@ get "/" do
   "OMG, hello Ruby Monstas!"
 end
 
+def store_name(filename,string)
+  File.open(filename,"a+") do |file|
+    file.puts(string)
+  end
+end
+
+def read_names
+  return [] unless File.exist?("names.txt")
+  File.read("names.txt").split("\n")
+end
+
+
 get "/monstas" do
   @name = params["name"]
+  @names = read_names
+  store_name("names.txt", @name)
   erb :monstas
 end

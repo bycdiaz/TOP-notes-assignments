296fc2912808efeda9b303589c5badedb2fa97f2
bycdiaz <cdiaz86@gmail.com>
2019-09-05 20:09:52 -0400

    Adds partials view for nav links.

--- ruby_on_rails/sinatra/jump-start-sinatra/ch2-building_basic_website/songs_by_sinatra/main.rb
+++ ruby_on_rails/sinatra/jump-start-sinatra/ch2-building_basic_website/songs_by_sinatra/main.rb
@@ -1,17 +1,22 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 
 get '/' do
+  @title = "Welcome!"
   erb :home
 end
 
 get '/about' do
+  @title = "All About This Website"
   erb :about
 end
 
 get '/contact' do
+  @title = "Write us!"
   erb :contact
 end
 
 not_found do
+  @title = "Whoops!"
   erb :not_found
 end
